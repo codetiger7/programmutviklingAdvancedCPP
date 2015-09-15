3ce9ebffe6cdc33a11a051bfb015a623e6f38502
codetiger7 <brianopedal@MacBook-Pro.local>
2015-09-15 13:33:56 +0200

    upload

--- day2/req1_date/date.h
+++ day2/req1_date/date.h
@@ -13,6 +13,23 @@ public:
    {
    }
 
+   bool isLeapYear()
+   {
+      if ( (year % 4 == 0))
+      {
+         if ( year % 100 == 0)
+         {
+            if (year % 400 == 0)
+               return true;
+
+            return false;
+         }
+         return true;
+      }
+
+      return false;
+   }
+
    int getDay()   const   { return day; }
    int getMonth() const { return month; }
    int getYear()  const  { return year; }
@@ -38,11 +55,71 @@ bool operator != (const Date& lhs, const Date& rhs)
 
 bool operator < (const Date& lhs, const Date& rhs)
 {
-   return (lhs.getYear() < rhs.getYear()) ||
-          (lhs.getMonth() < rhs.getMonth()) ||
-          (lhs.getDay() < rhs.getDay());
+   if (lhs == rhs)
+      return false;
+   else if ( lhs.getYear() < rhs.getYear())
+      return true;
+   else if (lhs.getYear() == rhs.getYear())
+   {
+      if (lhs.getMonth() < rhs.getMonth())
+         return true;
+      else if (lhs.getMonth() == rhs.getMonth())
+      {
+         if (lhs.getDay() < rhs.getDay())
+            return true;
+         else
+            return false;
+      }
+      else
+         return false;
+   }
+   else
+   {
+      return false;
+   }
 }
 
+
+bool operator > (const Date& lhs, const Date& rhs)
+{
+   if ( lhs != rhs)
+   {
+      if ( lhs.getYear() > rhs.getYear())
+         return true;
+      else if (lhs.getYear() == rhs.getYear())
+      {
+         if ( lhs.getMonth() > rhs.getMonth())
+            return true;
+         else if (lhs.getMonth() == rhs.getMonth())
+         {
+            if (lhs.getDay() > rhs.getDay())
+               return true;
+            else
+               return false;
+         }
+      }
+      else
+         return false;
+   }
+   return false;
+}
+
+
+bool operator <= (const Date& lhs, const Date& rhs)
+{
+   if (lhs == rhs)
+      return true;
+
+   return lhs < rhs;
+}
+
+bool operator >= (const Date& lhs, const Date& rhs)
+{
+   if (lhs == rhs)
+      return true;
+
+   return lhs > rhs;
+}
 
 #endif // DATE
 
--- day2/req1_date/main.cpp
+++ day2/req1_date/main.cpp
@@ -10,12 +10,57 @@ int main()
 
 
    Date d(15, 9, 2015);
+   Date d2(15,9,2015);
+
+   Date e(17,8,2015);
+   Date e2(17,8,2015);
+
+   Date a1(10, 10, 2010);
+   Date a2(9,10, 2010);
+
+   Date b(10, 12, 2010);
+   Date c(10,10,2009);
 
    // test gets
    cout << d.getDay()   << endl;
    cout << d.getMonth() << endl;
    cout << d.getYear()  << endl;
 
+   // equality tests
+   if ( d == d2)
+      cout << "Equality test 1 ok" << endl;
+   if (d == e)
+      cout << "Error in equality test 2" << endl;
+
+   // inequality test
+   if ( d != e)
+      cout << "Inequality test 1  ok" << endl;
+
+   if (d == e)
+      cout << "Error in Inequality test2" << endl;
+
+   // less than tests
+   cout << "less than == false == " << (a1 < a1) << endl;
+   cout << "less than == false == " << (a1 < a2) << endl;
+
+   // greater than tests
+   cout << "larger than == true == " << (b > c) << endl;
+   cout << "larger than == true == " << (a1 > a2) << endl;
+
+
+   // leap year tests
+   Date l1(10, 10, 1996);
+   Date l2(10,10,2000);
+
+   cout << "leap year true == " << l1.isLeapYear() << endl;
+   cout << "leap year true == " << l2.isLeapYear() << endl;
+
+
+   Date nl1(5,5,1900);
+   Date nl2(5,5,2001);
+   // false
+   cout << "leap year false == " << nl2.isLeapYear() << endl;
+   cout << "leap year false == " << nl2.isLeapYear() << endl;
 
 
 
