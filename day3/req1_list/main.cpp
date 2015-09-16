82142182c86f8e1cd489bb95f7844b0b57f63c88
codetiger7 <brianopedal@MacBook-Pro.local>
2015-09-16 13:17:37 +0200

    ver 16.

--- day3/req1_list/main.cpp
+++ day3/req1_list/main.cpp
@@ -1,3 +1,5 @@
+
+#include <cassert>
 #include <iostream>
 #include <string>
 #include "strlist.h"
@@ -19,22 +21,49 @@ int main()
    string d = "smile";
    string d1 = "smile";
 
+   // first deque
    StrList sList;
 
-   sList.ins(a);
-   sList.ins(b);
-   sList.ins(c);
+
+   sList.insertString(d);
+   sList.insertString(d);
+   sList.insertString(a);
+   sList.insertString(b);
+   sList.insertString(c);
 
    cout << c <<  " == " << sList.getLastAccess() << endl;
-   sList.ins(c);
-   sList.ins(c1);
-   sList.ins(b1);
+   sList.insertString(c);
+   sList.insertString(c1);
+   sList.insertString(a);
+   sList.insertString(d);
+   sList.insertString(a);
+   sList.insertString(b1);
    cout << b <<  " == " << sList.getLastAccess() << endl;
-
 
    cout << "\n\n\n";
    for( auto r : sList.getDeque())
       cout << r << endl;
+
+
+   // second deque
+   cout << "\n\n\n";
+   StrList l2(2);
+
+   l2.insertString(c);
+   l2.insertString(d);
+   l2.insertString(c);
+   l2.insertString(c);
+   l2.insertString(d);
+   l2.insertString(d);
+   l2.insertString(a);
+
+   assert( l2.getUpperLimit() == 2);
+
+   for( auto r : l2.getDeque())
+      cout << r << endl;
+
+
+
 
 
 
--- day3/req1_list/strlist.h
+++ day3/req1_list/strlist.h
@@ -4,38 +4,62 @@
 #include <algorithm>
 #include <deque>
 #include <string>
+#include <numeric>
 
 class StrList
 {
 public:
 
-   void ins(const std::string& str)
+   StrList() = default;
+
+   StrList(size_t maxSize)
+      : sList(maxSize)
+      , maxSz(maxSize)
+   {
+   }
+
+   void insertString(const std::string& str)
    {
       auto it = std::find(sList.begin(), sList.end(), str);
 
+      // already in list update order in list
       if (it != sList.end())
       {
          std::rotate(sList.begin(), it, it + 1);
       }
+      // reached max size remove least recently used and add new item
+      else if (sList.size() == maxSz)
+      {
+         sList.pop_back();
+         sList.push_front(str);
+      }
+      // new item
       else
          sList.push_front(str);
    }
+
+
+
 
    const std::deque<std::string>& getDeque() const
    {
       return sList;
    }
 
-   const std::string& getLastAccess()
+   const std::string& getLastAccess() const
    {
      return sList[0];
    }
 
 
+   size_t getUpperLimit() const
+   {
+      return maxSz;
+   }
 
 private:
    std::deque<std::string> sList;
-
+   const size_t maxSz = std::numeric_limits<size_t>::max();
 };
 
 #endif // STRLIST
