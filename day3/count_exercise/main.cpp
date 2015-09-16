9bd0b1b0cb0780a4e17cc8ecf7b4d221a4285c98
codetiger7 <brianopedal@MacBook-Pro.local>
2015-09-16 16:14:08 +0200

    ds

---
+++ day3/count_exercise/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cassert>
+
+template <typename T>
+class Counter
+{
+public:
+   Counter()
+   {
+      ++cnt;
+   }
+
+   ~Counter()
+   {
+      --cnt;
+   }
+
+   int static count()
+   {
+      return cnt;
+   }
+
+private:
+   static int cnt;
+};
+
+
+
+template<typename T>
+int Counter<T>::cnt = 0;
+
+
+
+
+class Y : public Counter<Y> {};
+class X : public Counter<X> {};
+
+using namespace std;
+
+int main()
+{
+   X x1;
+   {
+      X x2;
+   }
+   X x3;
+
+   Y y1;
+   Y y2;
+   {
+      Y y3;
+   }
+   Y y4;
+
+   assert(X::count() == 2);
+   assert(Y::count() == 3);
+
+
+   return 0;
+}
+
